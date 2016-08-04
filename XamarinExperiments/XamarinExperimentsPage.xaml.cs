7a94bbb578efc94841813390a0b9689c83914ce9
Andrew <0x384c0@gmail.com>
2016-08-04 17:07:21 +0600

    initial commit

---
+++ XamarinExperiments/XamarinExperimentsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinExperiments
+{
+	public partial class XamarinExperimentsPage : ContentPage
+	{
+		public XamarinExperimentsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
