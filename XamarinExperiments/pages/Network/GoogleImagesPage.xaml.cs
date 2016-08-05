d0e4c54086e7ff86fd16760fd4d38863274122ae
Andrew <0x384c0@gmail.com>
2016-08-05 17:22:00 +0600

    added reactive extensions

--- XamarinExperiments/pages/Network/GoogleImagesPage.xaml.cs
+++ XamarinExperiments/pages/Network/GoogleImagesPage.xaml.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reactive.Linq;
 
 using Xamarin.Forms;
 
 namespace XamarinExperiments {
 	public partial class GoogleImagesPage : ContentPage {
-		void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
-			Entry entry = (Entry) sender;
-			Debug.WriteLine(entry.Text);
-		}
+
+
+
 
 		public GoogleImagesPage() {
 			InitializeComponent();
+
+			Observable.FromEventPattern(this.searchEntry, "TextChanged")
+					  .Throttle(TimeSpan.FromMilliseconds(500))
+			          .Subscribe(inp => Debug.WriteLine("User wrote: " + ((Entry) inp.Sender).Text));
+
 			listView.ItemsSource = new CellData[]{
 				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 1",subtitle ="subtitle 1"},
 				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 2",subtitle ="subtitle 2"},
 				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 3",subtitle ="subtitle 3"},
 			};
+		}
+
+
+
+		private void Handle_TextChanged(string newValue) {
+			Debug.WriteLine(newValue);
 		}
 
 		public void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
