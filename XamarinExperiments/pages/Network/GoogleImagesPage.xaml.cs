c26ffd31bad60e585e9f61bc6d46364bf28eb262
Andrew <0x384c0@gmail.com>
2016-08-05 16:16:12 +0600

    added google images page

---
+++ XamarinExperiments/pages/Network/GoogleImagesPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace XamarinExperiments {
+	public partial class GoogleImagesPage : ContentPage {
+		void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+			Entry entry = (Entry) sender;
+			Debug.WriteLine(entry.Text);
+		}
+
+		public GoogleImagesPage() {
+			InitializeComponent();
+			listView.ItemsSource = new CellData[]{
+				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 1",subtitle ="subtitle 1"},
+				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 2",subtitle ="subtitle 2"},
+				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "image 3",subtitle ="subtitle 3"},
+			};
+		}
+
+		public void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
+			if (e.SelectedItem == null) return; // has been set to null, do not 'process' tapped event
+			((ListView)sender).SelectedItem = null; // de-select the row
+			Navigation.PushAsync(new GoogleImagesDetailPage());
+		}
+	}
+}
+
--- XamarinExperiments/pages/NetworkListPage.xaml.cs
+++ XamarinExperiments/pages/NetworkListPage.xaml.cs
@@ -5,11 +5,16 @@ using Xamarin.Forms;
 
 namespace XamarinExperiments {
 	public partial class NetworkListPage : ContentPage {
+		const string 
+		G_IMAGES_TITLE = "Google Images",
+		G_IMAGES_TITLE_NON_MODAL = "Google Images NON_MODAL";
+
+
 		public NetworkListPage() {
 			InitializeComponent();
 			listView.ItemsSource = new CellData[]{
-				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "title 1",subtitle ="subtitle 1"},
-				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "title 2",subtitle ="subtitle 2"},
+				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = G_IMAGES_TITLE,subtitle ="subtitle 1"},
+				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = G_IMAGES_TITLE_NON_MODAL,subtitle ="subtitle 2"},
 				new CellData { image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png", title = "title 3",subtitle ="subtitle 3"},
 			};
 		}
@@ -17,8 +22,22 @@ namespace XamarinExperiments {
 
 		public void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
 			if (e.SelectedItem == null) return; // has been set to null, do not 'process' tapped event
-			DisplayAlert("Tapped", ((CellData)e.SelectedItem).title + " row was tapped", "OK");
+			string title = ((CellData)e.SelectedItem).title;
 			((ListView)sender).SelectedItem = null; // de-select the row
+
+			switch (title) {
+				case G_IMAGES_TITLE:
+					Navigation.PushModalAsync(new NavigationPage(new GoogleImagesPage()));
+					break;
+					
+				case G_IMAGES_TITLE_NON_MODAL:
+					Navigation.PushAsync(new GoogleImagesPage());
+					break;
+
+				default:
+					DisplayAlert("Tapped", title + " row was tapped", "OK");
+					break;
+			}
 		}
 
 	}
